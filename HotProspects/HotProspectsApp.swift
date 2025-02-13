144c9608c4516ee4beb85ba3197d1ffcc1d0f259
Lochoa11 <linochoa11690@gmail.com>
2025-02-13 10:18:30 -0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Lin Ochoa on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HotProspectsTests/HotProspectsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsTests.swift
+//  HotProspectsTests
+//
+//  Created by Lin Ochoa on 2/13/25.
+//
+
+import Testing
+@testable import HotProspects
+
+struct HotProspectsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
