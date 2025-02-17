ed13f56f77802fa893ffb2b16d8e28165e984c46
Lochoa11 <linochoa11690@gmail.com>
2025-02-17 12:56:33 -0500

    adding code for SamplePackage dependencies on the last lesson on Day 81

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -5,17 +5,19 @@
 //  Created by Lin Ochoa on 2/13/25.
 //
 
+import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
+    let possibleNumbers = 1...60
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.formatted()
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(results)
     }
 }
 
